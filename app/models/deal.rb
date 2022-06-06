8b90dbd2a7c1db16b80ab842f0fb5261712aebc2
Batbayar Sukhbaatar <batbayar.sukhbaatar@klarna.com>
2022-06-06 02:21:41 +0200

    initialize seed data

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ApplicationRecord
   enum group: { promo: 'promo', percent: 'percent', absolute: 'absolute' }
-  belongs_to :product
-  belongs_to :product_size
+  belongs_to :product, optional: true
+  belongs_to :product_size, optional: true
 end
